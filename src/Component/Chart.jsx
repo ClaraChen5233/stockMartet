a28c4668aa449ef486bf48e4de18359a701f1094
ClaraChen5233 <chenchen5233@gmail.com>
2022-05-15 00:02:49 +1000

    delete

--- src/Component/Chart.jsx
+++ src/Component/Chart.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Chart as ChartJS, registerables } from "chart.js";
-import { Chart, Line } from "react-chartjs-2";
+import { Chart } from "react-chartjs-2";
 
 ChartJS.register(...registerables);
 
